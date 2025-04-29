28254b3d4fd7fd5c727dc6ce2847ce6b31ce370d
yassinechouch <yassinchouchene54@gmail.com>
2025-04-29 11:36:24 +0100

    aaa

--- nazihaproject/wwwroot/js/hub.js
+++ nazihaproject/wwwroot/js/hub.js
@@ -3,98 +3,89 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:5187/venteHub").build();
 
 // Function to get state text based on state code
-
-    
-
-
 function GetStateText(state) {
     switch (state) {
-        case 0: return "Initial";
-        case 1: return "en cours de traitement";
-        case 2: return "traité";
-        case 3: return "Refusé Center";
-        case 4: return "Refusé Terrain";
-        default: return "Unknown";
+        case 0:
+            return "Initial";
+        case 1:
+            return "en cours de traitement";
+        case 2:
+            return "traité";
+        default:
+            return "Unknown";
     }
 }
 
+// Function to get state color based on state code (you need to define this function)
 function GetStateColor(state) {
-    switch (state) {
-        case 0: return "darkblue-text";
-        case 1: return "orange-text";
-        case 2: return "green-text";
-        case 3: return "darkred-text";
-        case 4: return "red-text";
-        default: return "black-text";
-    }
-}
-
-function formatDate(date) {
-    var d = new Date(date);
-    var day = ('0' + d.getDate()).slice(-2);
-    var month = ('0' + (d.getMonth() + 1)).slice(-2);
-    var year = d.getFullYear();
-    return `${day}/${month}/${year}`;
+    // Implement this function according to your requirements
+    // Return appropriate CSS class or color based on the state code
 }
 
 connection.on("VenteDeleted", function (venteId) {
-    $("example2 tbody tr").each(function () {
+    // Find and remove the row with the corresponding venteId
+    $("#example2 tbody tr").each(function () {
         if ($(this).find("td:first").text().trim() === venteId.toString()) {
-            $(this).remove();
-            return false;
+            $(this).remove(); // Remove the row
+            return false; // Exit the loop after removing the row
         }
     });
 });
 
-connection.on("ReceiveUpdate", function (ventes) {
-    console.log("Received ventes:", ventes);
-    var table = $('#example2').DataTable();
-    table.clear().draw(); // Clear existing data
-    ventes.forEach(function (vente) {
-        addTableRow(vente);
-    });
+connection.on("ReceiveUpdate", function (vente) {
+    console.log("Received vente:", vente);
+    // Update the UI with the updated Vente data
+    updateTableRow(vente);
 });
 
 connection.on("NewVenteAdded", function (vente) {
     console.log("New Vente added:", vente);
-    addTableRow(vente);
+    // Update the UI with the new Vente data
+    updateTableRow(vente);
 });
 
-function addTableRow(vente) {
-    var formattedDate = formatDate(vente.dateEmission);
-    var newRow = '<tr class="odd ' + GetStateColor(vente.state) + ' blink">' +
-        '<td class="dtr-control sorting_1" tabindex="0">' + vente.id + '</td>' +
-        '<td>' + vente.cinNum + '</td>' +
-        '<td>' + formattedDate + '</td>' +
-        '<td>' + vente.type + '</td>' +
-        '<td>' + GetStateText(vente.state) + '</td>' +
-        '<td>' + (vente.reclamationCenter || '') + '</td>' +
-        '<td>' + (vente.reclamationTerrain || '') + '</td>' +
-        '<td>' + vente.contratNumber + '</td>' +
-        '<td>' + vente.articleId + '</td>' +
-        '<td>' +
-        '<form id="updateStateForm" action="/Ventes/UpdateStateToInProgress" method="post" style="display:inline-block;">' +
-        '<input type="hidden" name="venteId" value="' + vente.id + '" />' +
-        '<button type="submit" id="btnvalider">' +
-        '<i class="fas fa-info-circle"></i>' +
-        '</button>' +
-        '</form>' +
-        '<form action="/Ventes/DeleteVente" method="post" onsubmit="return confirmDelete()" style="display:inline-block;">' +
-        '<input type="hidden" name="venteId" value="' + vente.id + '" />' +
-        '<button type="submit" id="btnvalide">delete</button>' +
+function updateTableRow(vente) {
+    // Construct the new row HTML
+    
+    var newRow = '<tr class="odd ' + GetStateColor(vente.State) + '">' +
+        '<td class="dtr-control sorting_1" tabindex="0">' + vente.Id + '</td>' +
+        '<td>' + vente.cinNumber + '</td>' +
+        '<td>' + vente.DateEmission + '</td>' +
+        '<td>' + vente.Type + '</td>' +
+        '<td>' + GetStateText(vente.State) + '</td>' +
+        '<td>non traité</td>' +
+        '<td>22233365555554</td>' +
+        '<td>' + vente.ContratNumber + '</td>' +
+        '<td>' + vente.City + '</td>' +
+        '<td>' + vente.Country + '</td>' +
+        '<td>edawa5</td>' +
+        '<td> ' +
+        '<form id="updateStateForm" action="' + '@Url.Action("UpdateStateToInProgress", "Ventes")' + '" method="post">' +
+        '<input type="hidden" name="venteId" value="' + vente.Id + '" />' +
+        '<button type="submit" id="btnvalider">i</button>' +
         '</form>' +
         '</td>' +
         '</tr>';
 
-    $('#example2').DataTable().row.add($(newRow)).draw(); // Add new row
+    // Append the new row to the table body
+    $('#example2 tbody').append(newRow);
 }
+connection.on("NewPointageAdded", function (pointage) {
+    // Add the new Pointage to the table dynamically
+    var newRow = '<tr>' +
+        '<td>' + pointage.nom + '</td>' +
+        '<td>' + pointage.prénom + '</td>' +
+        '<td>' + pointage.Date_pointage + '</td>' +
+        '<td>' +
+        '<form method="post" id="updateStateForm" action="/Pointage/Pointage_detail">' +
+        '<input type="hidden" name="agent_terrein_id" id="agent_terrein_id" value="' + pointage.IdUserTerrain + '" />' +
+        '<button type="submit" id="btnvalider">i</button>' +
+        '</form>' +
+        '</td>' +
+        '</tr>';
 
+    $('#example3 tbody').append(newRow);
+});
 connection.start().then(function () {
     console.log('Connected to hub');
-}).catch(function (err) {
-    return console.error(err.toString());
 });
-
-function confirmDelete() {
-    return confirm("Êtes-vous sûr de vouloir supprimer cette vente ?");
-}
