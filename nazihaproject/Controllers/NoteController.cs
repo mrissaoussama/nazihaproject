765a37e4ca34b9867b76db56e5130ea40c9e6043
yassinechouch <yassinchouchene54@gmail.com>
2025-05-08 10:06:44 +0100

    aded note

---
+++ nazihaproject/Controllers/NoteController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using nazihaproject.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using NazihaProject.Data;
+
+namespace nazihaproject.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NoteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Note/Index - View all notes
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _context.Notes.ToListAsync();
+            return View(notes);
+        }
+
+        // GET: Note/Create - Show create form
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Note/Create - Add new note
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,LastName,Description")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(note);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+    }
+}
---
+++ nazihaproject/Migrations/20250507222007_AddNotesTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nazihaproject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
